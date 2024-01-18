df847886d9d8ab3047f7b342b36211c11d379556
Aniket <aniketraj28042003@gmail.com>
2024-01-19 01:20:04 +0530

    Added server to the project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const {PORT} = require("./config/serverConfig");
+const bodyParser = require("body-parser");
+
+const setupAndStartServer = async () => {
+
+    // create the express object
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+    });
+}
+
+setupAndStartServer();

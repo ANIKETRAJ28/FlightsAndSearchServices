7c5eb19ecfb860c58cf54b954fba19505282686f
Aniket <aniketraj28042003@gmail.com>
2024-01-20 15:49:15 +0530

    added update query to the city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -26,13 +26,26 @@ class CityRepository {
         }
     }
 
-    async getCity(cityId) {
+    async getCity(cityId) { // {id: cityId}
         try {
-            const city = await City.findByPk(cityId);
+            const city = await City.findByPk(cityId); // finds the city by primary key
             return city;
         } catch(error) {
             console.log("something went wrong in the repository layer");
             throw{error};
+        }
+    }
+
+    async updateCity(cityId, { data }) {
+        try {
+            const city = await City.update({ name: data }, {
+                where: {
+                  id: cityId
+                }
+            });
+            return city;
+        } catch(error) {
+            
         }
     }
 };
